63db1a92ba5b4997f33c14c5c8ac55b1f660363a
cc <joecc@umich.edu>
2021-03-11 15:33:14 -0600

    door_detection_update

--- yolov5/detect_l515_ros.py
+++ yolov5/detect_l515_ros.py
@@ -687,7 +687,7 @@ def detect(image, depth):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='best.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='best_corner.pt', help='model.pt path(s)')
     parser.add_argument('--source', type=str, default='data/images', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
